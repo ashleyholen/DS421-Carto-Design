cd2d51c26bc8abdced45563636e31c5dd84e36e9
ashleyholen <holen.ashley@gmail.com>
2025-08-10 23:13:52 -0500

    completed assignment 2

--- assignment2.R
+++ assignment2.R
@@ -1,65 +1,85 @@
 library(shiny)
-library(bslib)
 library(mapgl)
 library(sf)
 library(dplyr)
-library(viridis)
+library(viridisLite)
+library(classInt)
 library(here)
 
-# Load county data
 county_data <- st_read(here("data/county_data.gpkg"))
+st_geometry(county_data) <- "geom"
+county_data <- st_transform(county_data, 4326)
+county_data <- st_make_valid(county_data)
 
-ui <- page_sidebar(
-  title = "Language Map by County",
-  sidebar = sidebar(
-    selectInput(
-      "language",
-      "Select Language:",
-      choices = sort(unique(county_data$language)),
-      selected = "Spanish"
+ui <- fluidPage(
+  titlePanel("Language Map by County"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        "language",
+        "Select Language:",
+        choices = sort(unique(county_data$language)),
+        selected = "Spanish"
+      )
+    ),
+    mainPanel(
+      maplibreOutput("map", height = "700px")
     )
-  ),
-  card(
-    full_screen = TRUE,
-    maplibreOutput("map")
   )
 )
 
 server <- function(input, output, session) {
-  
   output$map <- renderMaplibre({
+    req(input$language)
     
-    # Filter to selected language
-    lang_data <- county_data %>%
-      filter(language == input$language)
+    filtered <- county_data %>% filter(language == input$language)
     
-    # Make sure geometries are valid
-    lang_data <- st_make_valid(lang_data)
+    summary_data <- filtered %>%
+      group_by(GEOID, geoname) %>%
+      summarize(percent_speakers = sum(percent_speakers, na.rm = TRUE), .groups = "drop")
     
-    # Create color column from viridis
-    pal <- colorNumeric(viridis(256), domain = lang_data$percent_speakers)
-    lang_data <- lang_data %>%
-      mutate(
-        fill_col = pal(percent_speakers),
-        popup_text = paste0(
-          "<b>", geoname, "</b><br>",
-          "Percent Speakers: ", round(percent_speakers, 2), "%"
-        )
-      )
+    geom_data <- filtered %>%
+      distinct(GEOID, geom)
     
-    # Build map
+    geom_data_df <- sf::st_drop_geometry(geom_data)
+    
+    lang_sf <- summary_data %>%
+      left_join(geom_data_df, by = "GEOID") %>%
+      st_as_sf(sf_column_name = "geom") %>%
+      st_make_valid()
+    
+    req(nrow(lang_sf) > 0)
+    
+    # Create reversed magma palette function
+    reversed_inferno <- function(n) {
+      rev(viridisLite::inferno(n))
+    }
+    
+    continuous_scale <- interpolate_palette(
+      data = lang_sf,
+      column = "percent_speakers",
+      method = "equal",
+      n = 7,
+      palette = reversed_inferno
+    )
+    
     maplibre(style = carto_style("positron")) |>
-      fit_bounds(lang_data, animate = FALSE) |>
+      fit_bounds(lang_sf, animate = FALSE) |>
       add_fill_layer(
         id = "lang_layer",
-        source = lang_data,       # full sf object
-        fill_color = "fill_col",  # matches column name
-        fill_opacity = 0.7,
-        popup = "popup_text"
+        source = lang_sf,
+        fill_color = continuous_scale$expression,
+        fill_opacity = 0.8,
+        fill_outline_color = "#444444"
+      ) |>
+      add_legend(
+        "Percent Speakers",
+        values = get_legend_labels(continuous_scale),
+        colors = get_legend_colors(continuous_scale),
+        type = "continuous"
       )
   })
 }
 
 shinyApp(ui, server)
 
-        
