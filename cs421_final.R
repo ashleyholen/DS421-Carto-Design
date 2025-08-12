ca49f6625e32b7c238f3f6e4f19f8f3f73c83eb8
ashleyholen <holen.ashley@gmail.com>
2025-08-12 16:03:50 -0500

    map rendering, points not

--- cs421_final.R
+++ cs421_final.R
@@ -1,34 +1,63 @@
+library(shiny)
+library(mapgl)
 library(sf)
-library(dplyr)
-library(ggplot2)
 library(here)
+library(dplyr)
 
-# Read data
-oil_data <- st_read(here("data/OGD_Rigs/OGD_Rigs.shp"))
-gas_data <- st_read(here("data/OGD_GasPlants/OGD_GasPlants.shp"))
-wind_data <- st_read(here("data/NDGISHUB_Wind_Turbines/Wind_Turbines.shp"))
-coal_data <- st_read(here("data/Surface_and_Underground_Coal_Mines_in_the_US/CoalMines_US_EIA.shp"))
-
-# Reproject all datasets to EPSG:4326
 common_crs <- 4326
-oil_data <- st_transform(oil_data, crs = common_crs)
-gas_data <- st_transform(gas_data, crs = common_crs)
-wind_data <- st_transform(wind_data, crs = common_crs)
-coal_data <- st_transform(coal_data, crs = common_crs)
 
-# Filter coal data to North Dakota
-coal_data <- coal_data %>% filter(state == "NORTH DAKOTA")
+# Load and transform datasets
+oil_data <- st_read(here("data/OGD_Rigs/OGD_Rigs.shp")) %>% st_transform(common_crs)
+gas_data <- st_read(here("data/OGD_GasPlants/OGD_GasPlants.shp")) %>% st_transform(common_crs)
+wind_data <- st_read(here("data/NDGISHUB_Wind_Turbines/Wind_Turbines.shp")) %>% st_transform(common_crs)
+coal_data <- st_read(here("data/Surface_and_Underground_Coal_Mines_in_the_US/CoalMines_US_EIA.shp")) %>%
+  st_transform(common_crs) %>%
+  filter(state == "NORTH DAKOTA")
 
-# Plot all datasets
-ggplot() +
-  geom_sf(data = oil_data, color = "orange", size = 2) +
-  geom_sf(data = gas_data, color = "red", size = 2) +
-  geom_sf(data = wind_data, color = "blue", size = 0.4, alpha = 0.6) +
-  geom_sf(data = coal_data, color = "black", size = 1.2) +
-  theme_minimal() +
-  labs(
-    title = "Energy Infrastructure in North Dakota",
-    subtitle = "Oil Rigs, Gas Plants, Wind Turbines, and Coal Mines"
+datasets <- list(
+  "Oil Rigs" = oil_data,
+  "Gas Plants" = gas_data,
+  "Wind Turbines" = wind_data,
+  "Coal Mines" = coal_data
+)
+
+ui <- fluidPage(
+  titlePanel("Energy Infrastructure in North Dakota"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("dataset", "Select dataset to display:", choices = names(datasets), selected = "Oil Rigs")
+    ),
+    mainPanel(
+      maplibreOutput("map", height = "800px")
+    )
   )
+)
 
+server <- function(input, output, session) {
+  output$map <- renderMaplibre({
+    req(input$dataset)
+    sf_data <- datasets[[input$dataset]]
+    
+    layer_color <- switch(input$dataset,
+                          "Oil Rigs" = "orange",
+                          "Gas Plants" = "red",
+                          "Wind Turbines" = "blue",
+                          "Coal Mines (ND)" = "black",
+                          "gray"
+    )
+    
+    maplibre(style = carto_style("positron")) %>%
+      fit_bounds(sf_data, animate = FALSE) %>%
+      add_vector_source("selected_source", sf_data) %>%
+      add_circle_layer(
+        id = "selected_layer",
+        source = "selected_source",
+        circle_color = layer_color,
+        circle_radius = 8,
+        circle_opacity = 0.8
+      )
+  })
+}
+
+shinyApp(ui, server)
 
